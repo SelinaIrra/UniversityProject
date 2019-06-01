988fb837367453f6d31056ff4d3b3832f3d1b8f9
SelinaIrra <irra.selina@mail.ru>
2019-06-01 10:59:31 +0400

    handle news list getter parameters

--- src/routes/news.js
+++ src/routes/news.js
@@ -5,14 +5,23 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     const params = req.query;
-    NEWS.find({}, (err, arr) => {
+    params.count = params.count ? Number(params.count) : 10;
+    params.offset = params.offset ? Number(params.offset) : 0;
+    if (!(params.count > 0 && params.offset >= 0)) {
+        return res.status(400).json();
+    }
+    NEWS.find({}, null, {
+        sort: '-date',
+        limit: params.count,
+        skip: params.offset,
+    }, (err, arr) => {
         if (err) {
             console.error(err);
             res.status(500).json();
             return;
         }
         res.json({
-            offset: params.offset || null,
+            offset: params.offset,
             count: arr.length,
             data: arr.map(x => ({
                 id: x.id,
@@ -30,7 +39,7 @@ router.get('/:id', function (req, res, next) {
     NEWS.findById(req.params.id, (err, single_new) => {
         if (err) {
             if (err.kind === "ObjectId")
-                return res.status(204).json({});
+                return res.status(204).json();
             console.error(err);
             return res.status(500).json();
         }
