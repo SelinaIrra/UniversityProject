b9eee27618a0dbb2c01bb8935b348ad1a57f79d1
SelinaIrra <irra.selina@mail.ru>
2019-06-01 11:57:24 +0400

    make db connection async

--- src/db.js
+++ src/db.js
@@ -6,21 +6,23 @@ const mongoDB = `mongodb://${CONFIG.host}:${CONFIG.port}/${CONFIG.database}`;
 
 let db = null;
 
-function get_connection() {
+async function get_connection() {
     if (!db) {
         console.log(`Connecting mongo on "${mongoDB}"`);
 
-        mongoose.connect(mongoDB, {useNewUrlParser: true}).then(() => {
-            console.log("Connected");
-        }).catch((err) => {
-            console.error("Failed to connect", err);
+        try {
+            await mongoose.connect(mongoDB, {useNewUrlParser: true});
+            console.log("MongoDB connected");
+            db = mongoose.connection;
+        } catch (err) {
+            console.error("Failed to connect to mongo", err);
             process.exit(2);
-        });
-
-        db = mongoose.connection;
-        mongoose.Promise = global.Promise;
+        }
     }
     return db;
 }
 
-module.exports = get_connection();
+module.exports = get_connection().catch((err) => {
+    console.error("Failed to connect to mongo", err);
+    process.exit(2);
+});
