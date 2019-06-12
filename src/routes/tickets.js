7b339a9d28b57bfdf926d4acf308929c16709934
SelinaIrra <irra.selina@mail.ru>
2019-06-12 20:29:27 +0400

    tickets get method added

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const send_mail = require('../mail');
 const TICKETS = require('../models/tickets');
+const check_auth = require('../utils').check_auth;
 
 const required_fields = ["name", "surname", "phone", "school", "class", "faculty"];
 const router = express.Router();
@@ -8,6 +9,41 @@ const router = express.Router();
 function validate(object) {
     return required_fields.every(x => object[x]) && String(object.phone).length === 11 && Number(object.phone);
 }
+
+router.get('/', function (req, res) {
+    const auth_status = check_auth(req);
+    if (auth_status !== 200)
+        return res.set('WWW-Authenticate', 'Basic realm="401"').status(auth_status).json({});
+
+    const params = req.query;
+    params.count = params.count ? Number(params.count) : 10;
+    params.offset = params.offset ? Number(params.offset) : 0;
+    if (!(params.count > 0 && params.offset >= 0)) {
+        return res.status(400).json({});
+    }
+    TICKETS.find({}, null, {
+        sort: '-date',
+        limit: params.count,
+        skip: params.offset,
+    }, (err, arr) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({});
+        }
+        res.json({
+            offset: params.offset,
+            count: arr.length,
+            data: arr.map(x => { return {
+                id: x.id,
+                fio: x.fio,
+                school: x.school,
+                class: x.class,
+                faculty: x.faculty,
+                phone: x.phone,
+            }})
+        });
+    });
+});
 
 router.post('/', function (req, res, next) {
     const params = req.body;
