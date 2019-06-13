1d2359f6aaaddbc9de0e08a4c17b75c914b3f53e
irinaS <irra.selina@mail.ru>
2019-06-13 12:59:31 +0400

    add team work

---
+++ src/public/scripts/gallery.js
@@ -0,0 +1,4 @@
+﻿$('#thumbs').delegate('img','click', function(){
+    $('#largeImage').attr('src',$(this).attr('src').replace('thumb','large'));
+    $('#description').html($(this).attr('alt'));
+});
